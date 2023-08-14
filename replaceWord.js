6f8e04233a26421bc57178c7b15588ea9b2bf521
مُحَمَّد <mhmdnoman>
2023-08-14 14:04:50 +0600

    added solution

--- replaceWord.js
+++ replaceWord.js
@@ -1,11 +1,15 @@
 // Replace a specific word in a string with another word.
 
 function replaceWord(originalStr, wordToReplace, replacementWord) {
-	let modifiedString = originalStr.replace( new RegExp(wordToReplace, 'g'), replacementWord);
+	let modifiedString = originalStr.replace(
+		new RegExp(wordToReplace, "g"),
+		replacementWord,
+	);
 	return modifiedString;
 }
 
-const originalStr = "Hello Earth! Javascript is awesome. Hello Earth! is our first programm.";
+const originalStr =
+	"Hello Earth! Javascript is awesome. Hello Earth! is our first programm.";
 const wordToReplace = "Earth";
 const replacementWord = "World";
 

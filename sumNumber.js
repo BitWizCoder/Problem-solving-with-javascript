d7ed89a4fbde6bdb76f738ed181f7eacf22a42cf
coderamrin <coderamrin@gmail.com>
2023-09-22 17:24:07 +0600

    removed solution files

--- sumNumber.js
+++
@@ -1,8 +0,0 @@
-// Create a simple function that returns the sum of two numbers.
-
-function sumNumber(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
-}
-
-console.log(sumNumber(4, 10)); // Output : 14
